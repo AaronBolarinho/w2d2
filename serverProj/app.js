9787d0d1a180d2dd0516fed74693d310739052c8
AaronBolarinho <aaron.bolarinho@gmail.com>
2018-10-02 19:14:09 +0000

    urls successfully displaying in browser at /urls

--- serverProj/app.js
+++ serverProj/app.js
@@ -1,20 +1,60 @@
-const http = require("http");
-const PORT = 8080;
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
-// a function which handles requests and sends response
-function requestHandler(request, response) {
-  if (request.url == "/") {
-    response.end("Welcome!");
-  } else if (request.url == "/urls") {
-    response.end("www.lighthouselabs.ca\nwww.google.com");
-  } else {
-    response.statusCode = 404;
-    response.end("Unknown Path");
-  }
+let templateVars = { urls: urlDatabase };
+
+for(key in templateVars) {
+  for(key in urlDatabase) {
+  console.log(key);
+  console.log(urlDatabase[key])
 }
+};
 
-var server = http.createServer(requestHandler);
 
-server.listen(PORT, () => {
-  console.log(`Server listening on: http://localhost:${PORT}`);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const http = require("http");
+// const PORT = 8080;
+
+// // a function which handles requests and sends response
+// function requestHandler(request, response) {
+//   if (request.url == "/") {
+//     response.end("Welcome!");
+//   } else if (request.url == "/urls") {
+//     response.end("www.lighthouselabs.ca\nwww.google.com");
+//   } else {
+//     response.statusCode = 404;
+//     response.end("Unknown Path");
+//   }
+// }
+
+// var server = http.createServer(requestHandler);
+
+// server.listen(PORT, () => {
+//   console.log(`Server listening on: http://localhost:${PORT}`);
+// });
+
+/* test code
+
+<!--  <ul>
+
+  <% urlDatabase.forEach(function(oneUrl) { %>
+  <li><a href='#'></a></li>
+  <% }) %>
+ </ul> -->
+
+ */
