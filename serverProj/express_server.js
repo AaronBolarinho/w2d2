1adbb7f83ec391a94ec26ed984d6ab7848a0df4a
AaronBolarinho <aaron.bolarinho@gmail.com>
2018-10-07 01:17:23 +0000

    code cleanup done - only readme left to do

--- serverProj/express_server.js
+++ serverProj/express_server.js
@@ -4,21 +4,17 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-// var cookieParser = require('cookie-parser')
 var cookieSession = require('cookie-session')
 const bcrypt = require('bcrypt');
-
 var app = express()
-// app.use(cookieParser())
-app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 app.use(cookieSession({
   name: 'session',
   keys: ['avastYeSwarthyLubbers'],
   maxAge: 24 * 60 * 60 * 1000
 }))
-
-app.set("view engine", "ejs");
 
 function generateRandomString() {
   let r = Math.random().toString(36).substring(7);
@@ -33,24 +29,22 @@ function currentUser(req) {
 
 // These are my global objects
 
-var urlDatabase = {default: {"b2xVn2": "http://www.lighthouselabs.ca",
-                              "9sm5xK": "http://www.google.com"}
+var urlDatabase = {default: {"b2xVn2": "http://www.lighthouselabs.ca"}
 };
 
 const users = {
-  "userRandomID": {
-    id: "userRandomID",
+  "testUserID": {
+    id: "testUserID",
     email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
- "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
 }
 
 // These are my app.gets
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 app.get("/error", (req, res) => {
   return res.render("urls_error");
@@ -63,32 +57,18 @@ app.get("/login", (req, res) => {
 app.get("/", (req, res) => {
 
   if(currentUser(req)) {
-      res.redirect("/urls");
-      return;
-    }
-
+    res.redirect("/urls");
+    return;
+  }
   return res.render("_hello");
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
-app.get("/urls.json", (req, res) => {
-  return res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  return res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/register", (req, res) => {
 
   if(currentUser(req)) {
-      res.redirect("/urls");
-      return;
-    }
-
+    res.redirect("/urls");
+    return;
+  }
   let templateVars = {
     user : currentUser(req)
   }
@@ -101,14 +81,10 @@ app.get("/urls", (req, res) => {
     res.render("urls_error");
     return;
   }
-
   let myUserId = currentUser(req).id
-
   let templateVars = { urls: urlDatabase,
                        user : currentUser(req),
-                       myUserId : myUserId
-                     };
-
+                       myUserId : myUserId};
   return res.render("urls_index", templateVars);
 });
 
@@ -118,11 +94,9 @@ app.get("/urls/new", (req, res) => {
     res.render("urls_error");
     return;
   }
-
   let templateVars = {
     user : currentUser(req)
   }
-
   return res.render("urls_new", templateVars);
 });
 
@@ -132,16 +106,13 @@ if(!currentUser(req)) {
     res.render("urls_error");
     return;
   }
-
   let longURL;
-
   for(var key1 in urlDatabase){
     for(var key2 in urlDatabase[key1]){
       if(key2 = req.params.shortURL)
-      longURL = urlDatabase[key1][key2]
+      longURL = urlDatabase[key1][key2];
     }
   }
-
   return res.redirect(longURL);
 });
 
@@ -157,16 +128,14 @@ app.get("/urls/:id", (req, res) => {
   for(var key1 in urlDatabase){
     for(var key2 in urlDatabase[key1]){
       if(key2 = req.params.id)
-      longURL = urlDatabase[key1][key2]
+      longURL = urlDatabase[key1][key2];
     }
   }
 
   let templateVars = {
-  shortURL: req.params.id,
-  longUrl : longURL,
-  user : currentUser(req)
-  }
-
+    shortURL: req.params.id,
+    longUrl : longURL,
+    user : currentUser(req)};
   return res.render("urls_show", templateVars);
 })
 
@@ -186,12 +155,10 @@ app.post("/urls", (req, res) => {
     urlDatabase[currentUser(req).id] = {[randomNum]: req.body.longURL};
   }
 
-  console.log(urlDatabase);
   return res.redirect("http://localhost:8080/urls/");
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-
 var userKey = currentUser(req).id;
 
   for(var key in urlDatabase[userKey]) {
@@ -209,7 +176,7 @@ app.post("/urls/:id", (req, res) => {
   for(var key1 in urlDatabase){
     for(var key2 in urlDatabase[key1]){
       if(key2 = req.params.id)
-      urlDatabase[key1][key2] = req.body.longURL
+      urlDatabase[key1][key2] = req.body.longURL;
     }
   }
 
@@ -219,30 +186,17 @@ app.post("/urls/:id", (req, res) => {
 app.post("/login", (req, res) => {
 
   if (req.body.email && req.body.password) {
-     console.log('theres values', req.body.email, req.body.password)
     for (let key in users) {
-       console.log('looping through', key, users[key])
       if (req.body.email === users[key].email) {
-         console.log('email ok')
         if(bcrypt.compareSync(req.body.password, users[key].password)) {
-           console.log('pwd ok')
-
-            req.session.user_id = users[key]
-            console.log(users);
-;
+          req.session.user_id = users[key];
           return res.redirect("http://localhost:8080/urls");
         } else {
           return res.status(403).send('Arrrr...wrong pwd!');
         }
       }
     }
-
-
-
-    // console.log('no users matched')
-
     return res.status(404).send('Arrrr...ye ought to register first, Mate-y!');
-
   } else {
     return res.status(400).send('Arrrr...ye mis-typed yer email or password!');
   }
@@ -250,7 +204,6 @@ app.post("/login", (req, res) => {
 
 app.post("/logout", (req, res) => {
   req.session = null;
-     console.log(users);
   return res.redirect("http://localhost:8080/");
 });
 
@@ -258,29 +211,21 @@ app.post("/register", (req, res) => {
 
   if(!req.body.email || !req.body.password) {
     return res.status(400).send('Avast! You forgot to give your name or password!');
-  }
+  };
 
   for (let key in users) {
     if(req.body.email === users[key].email) {
       return res.status(400).send('Arrrr...ye already registered! Do you want to login?');
-      // suggest redirection to login
     }
-  }
+  };
 
   let randomNum = generateRandomString();
-
   const hashedPassword = bcrypt.hashSync(req.body.password, 10);
 
    users[randomNum] = { id: randomNum,
                        email: req.body.email,
-                       password: hashedPassword
-                      }
+                       password: hashedPassword};
   req.session.user_id = randomNum;
-  // res.cookie('user_id', randomNum)
-  console.log("These are the current users", users);
-  console.log(currentUser(req));
-  console.log(req.session.user_id);
-  console.log(users);
   return res.redirect("http://localhost:8080/login");
 });
 
